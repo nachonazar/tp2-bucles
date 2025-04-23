4680d1398799a0af8f91368cb6ca680c62678b85
Ignacio <ignacionazar9@gmail.com>
2025-04-23 15:53:26 -0300

    agregue el ejercicio 15

---
+++ bucles15.js
@@ -0,0 +1,183 @@
+const texto = prompt("Ingrese un texto (maximo 20 caracteres)").toLowerCase();
+let vocales = 0;
+if (
+  texto.charAt(0) === "a" ||
+  texto.charAt(0) === "e" ||
+  texto.charAt(0) === "i" ||
+  texto.charAt(0) === "o" ||
+  texto.charAt(0) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(1) === "a" ||
+  texto.charAt(1) === "e" ||
+  texto.charAt(1) === "i" ||
+  texto.charAt(1) === "o" ||
+  texto.charAt(1) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(2) === "a" ||
+  texto.charAt(2) === "e" ||
+  texto.charAt(2) === "i" ||
+  texto.charAt(2) === "o" ||
+  texto.charAt(2) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(3) === "a" ||
+  texto.charAt(3) === "e" ||
+  texto.charAt(3) === "i" ||
+  texto.charAt(3) === "o" ||
+  texto.charAt(3) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(4) === "a" ||
+  texto.charAt(4) === "e" ||
+  texto.charAt(4) === "i" ||
+  texto.charAt(4) === "o" ||
+  texto.charAt(4) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(5) === "a" ||
+  texto.charAt(5) === "e" ||
+  texto.charAt(5) === "i" ||
+  texto.charAt(5) === "o" ||
+  texto.charAt(5) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(6) === "a" ||
+  texto.charAt(6) === "e" ||
+  texto.charAt(6) === "i" ||
+  texto.charAt(6) === "o" ||
+  texto.charAt(6) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(7) === "a" ||
+  texto.charAt(7) === "e" ||
+  texto.charAt(7) === "i" ||
+  texto.charAt(7) === "o" ||
+  texto.charAt(7) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(8) === "a" ||
+  texto.charAt(8) === "e" ||
+  texto.charAt(8) === "i" ||
+  texto.charAt(8) === "o" ||
+  texto.charAt(8) === "u"
+) {
+  vocales += 1;
+}
+if (
+  texto.charAt(9) === "a" ||
+  texto.charAt(9) === "e" ||
+  texto.charAt(9) === "i" ||
+  texto.charAt(9) === "o" ||
+  texto.charAt(9) === "u"
+) {
+  vocales += 1;
+}
+if (
+    texto.charAt(10) === "a" ||
+    texto.charAt(10) === "e" ||
+    texto.charAt(10) === "i" ||
+    texto.charAt(10) === "o" ||
+    texto.charAt(10) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(11) === "a" ||
+    texto.charAt(11) === "e" ||
+    texto.charAt(11) === "i" ||
+    texto.charAt(11) === "o" ||
+    texto.charAt(11) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(12) === "a" ||
+    texto.charAt(12) === "e" ||
+    texto.charAt(12) === "i" ||
+    texto.charAt(12) === "o" ||
+    texto.charAt(12) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(13) === "a" ||
+    texto.charAt(13) === "e" ||
+    texto.charAt(13) === "i" ||
+    texto.charAt(13) === "o" ||
+    texto.charAt(13) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(14) === "a" ||
+    texto.charAt(14) === "e" ||
+    texto.charAt(14) === "i" ||
+    texto.charAt(14) === "o" ||
+    texto.charAt(14) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(15) === "a" ||
+    texto.charAt(15) === "e" ||
+    texto.charAt(15) === "i" ||
+    texto.charAt(15) === "o" ||
+    texto.charAt(15) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(16) === "a" ||
+    texto.charAt(16) === "e" ||
+    texto.charAt(16) === "i" ||
+    texto.charAt(16) === "o" ||
+    texto.charAt(16) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(17) === "a" ||
+    texto.charAt(17) === "e" ||
+    texto.charAt(17) === "i" ||
+    texto.charAt(17) === "o" ||
+    texto.charAt(17) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(18) === "a" ||
+    texto.charAt(18) === "e" ||
+    texto.charAt(18) === "i" ||
+    texto.charAt(18) === "o" ||
+    texto.charAt(18) === "u"
+  ) {
+    vocales += 1;
+  }
+  if (
+    texto.charAt(19) === "a" ||
+    texto.charAt(19) === "e" ||
+    texto.charAt(19) === "i" ||
+    texto.charAt(19) === "o" ||
+    texto.charAt(19) === "u"
+  ) {
+    vocales += 1;
+  }
+document.writeln("Vocales encontradas: " + vocales);
